dd0f6ab01af58b753bd48874954a87ee4ab00abd
axg0516 <avinash.gandhasiri@dexcom.com>
2022-01-30 11:50:42 -0800

    added project domain and repository

---
+++ src/main/java/com/avisan/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.avisan.ppmtool.repositories;
+
+import com.avisan.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
